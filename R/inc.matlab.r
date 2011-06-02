918bd90f8458764bf5941a45be1290a817dceb80
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2011-06-02 16:59:47 +0100

    added a spread function for multidimensional arrays

--- R/inc.matlab.r
+++ R/inc.matlab.r
@@ -6,6 +6,42 @@ repmat <- function(A,n,m) {
   return( array(A ,dim = d)) 
 }
 
+
+# spread the array A[1,2,3] to 
+# what dim specifies in the order given
+# spread(A , c(2) , 10) will insert a dimension
+# in the second index of size 10
+spread <- function(A, loc, dims) {
+  adims = dim(A)
+  
+  # check dimensions
+  l = length(loc)
+  if (max(loc)> length(dim(A))+l) { 
+    stop('incorrect dimensions in spread')
+  }
+
+  # total dimension not in order
+  sdim = c(dim(A),dims)
+
+  # permutation of dims
+  edim = c()
+  oi =1        # original dims
+  ni =length(dim(A))+1 # new dims
+  for (i in c(1: (length(dim(A))+l))) {
+    if (i %in% loc) {
+      edim = c(edim,ni)
+      ni = ni+1
+    } else {
+      edim = c(edim,oi)
+      oi = oi +1
+    }
+  }
+
+  return( aperm( array(A,dim=sdim),edim)) 
+}
+
+
+
 library(rgl)
 mesh <- function(x=(1:nrow(Z)) / nrow(Z) ,y=(1:nrow(Z)) / nrow(Z),Z) {
 	
@@ -22,9 +58,4 @@ mesh <- function(x=(1:nrow(Z)) / nrow(Z) ,y=(1:nrow(Z)) / nrow(Z),Z) {
 	#rgl.surface(x,y,z)
 }
 
-
-# spreads an array
-spread <- function(A,c) {
-  
-}
 
