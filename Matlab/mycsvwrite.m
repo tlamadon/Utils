56584485bb6fc3a7e028baa312e76f2bc499d429
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2011-07-18 14:45:01 -0700

    Edited Matlab/mycsvwrite.m via GitHub

--- Matlab/mycsvwrite.m
+++ Matlab/mycsvwrite.m
@@ -2,6 +2,8 @@ function mycsvwrite(filename,var,header)
 % writes a CSV file with headers
 % mycsvwrite(filename,var,header)
 % header is a comma separeted list of names
+% for example, if you have a 25x4 matrix M you can do:
+%   mycsvwrite('datafile.csv',M,'id,wage,age,educ')
 
 outid = fopen(filename, 'w+');
 fprintf(outid, '%s\n', header);
