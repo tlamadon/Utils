b004a142eb07f798d66b00160468e58c35c3cda5
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2011-08-24 13:16:59 -0400

    added functions to append new variable to long formatted tables, and a function to merge long formated tables

--- R/fun.mdata.r
+++ R/fun.mdata.r
@@ -18,28 +18,69 @@
 # 4) combining when some columns are missing (but I guess this is a sort 
 # of merge)
 
+mappend <- function(d,variable,value) {
+  non_cst_var = c()
+  for (v in names(d)) {
+    if ( length(unique(d[,v]))>1) non_cst_var = c(non_cst_var,v);
+  }
+
+  new_line = d[1,]
+  new_line[,non_cst_var] = NA
+  new_line$value[1] = value
+  new_line$variable = variable
+  d = rbind(d,new_line)
+
+  return(d)
+}
+
+
+
+
 # this first function moves a given var fr
 # this function will merge data1 with data2
 # using as id variable 
-# mmerge <- function(data1,data2,by,vars) {
-# 
-# 
-#   # first we find 
-#   # first we cast the data to get the byvar in columns
-#   data1 = 
-#   
-# 
-#   # the ddfun selects the vars 
-#   ddfun <- function(d) {
-#     # find from data2, the line that matches in byvar
-#     
-# 
-# 
-#   }
-# 
-#   # we run a ddply per each byid
-# 
-# }
+mmerge <- function(data1,data2,idvar,byvar,vars) {
+  
+  # 1) we find which vars are ids and which vars
+  # are variables
+  varid = setdiff(names(data1),c('variable','value'))
+
+  byid       = intersect(varid, byvar)
+  byvariable = setdiff(byvar, byvar) 
+
+  # next we run a ddply per byid
+  nd = ddply(data1,idvar,function(d) {
+
+    # we need to construct a selector
+    I = seq(1,l=nrow(data2))
+
+    # select only if value if equal
+    for ( v in byvariable ) {
+      I = I & (data2[,v] == d$value[variable == v])
+    }
+    for ( v in byid ) {
+      I = I & (data2[,v] == d[1,v])
+    }
+   
+    # find variable that remain constant over d
+    non_cst_var = c()
+    for (v in names(d)) {
+      if ( length(unique(d[,v]))>1) non_cst_var = c(non_cst_var,v);
+    }
+
+    # finally we append the value of the I row to the data frame
+    for ( v in vars) {
+      new_line = d[1,]
+      new_line[,non_cst_var] = NA
+      new_line$value[1] = data2[I,v]
+      new_line$variable = v 
+      d = rbind(d,new_line)
+    }
+    return(d)
+  })
+
+  return(nd)
+}
 
 # #todo deal with factors as well!
 renameany <-function(d,rules) {
